46ef9d7823294177c9cdf7850741f929bc540bf6
EX0PH1US <ex0ph1us2007@gmail.com>
2026-06-19 18:09:19 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    sku: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    lowStockThreshold: {
+        type: Number,
+        min: 0,
+        default: 5
+    }
+})
