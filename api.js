c6bc5240356f599a81bea6b585215522d8dc34e6
EX0PH1US <ex0ph1us2007@gmail.com>
2026-06-23 01:41:24 +0530

    added CORS and helmet

--- api.js
+++ api.js
@@ -8,6 +8,7 @@ import errorHandler from "./middleware/errorHandler.js"
 import authRouter from "./routes/auth.js"
 import cookieParser from "cookie-parser"
 import expressRateLimiter from "express-rate-limiter"
+import helmet from "helmet"
 
 const app = express()
 
@@ -22,13 +23,21 @@ const rateLimit = expressRateLimiter({
     legacyHeaders: false
 })
 
+app.use(cors({
+    origin: process.env.FRONT,
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: ['Content-type', 'Authorization'],
+    credentials: true
+}))
+
+app.use(helmet())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(rateLimit)
 
 
-app.use(morgon('dev'))
+app.use(morgon('common'))
 
 app.use('/', inventoryRouter)
 app.use('/', authRouter)
@@ -43,5 +52,5 @@ try {
 app.use(errorHandler)
 
 app.listen(3000, () => {
-    console.log("http://localhost:3000")
+    console.log("Working...")
 })
