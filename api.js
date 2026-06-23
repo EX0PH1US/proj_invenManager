b3a26aaf58437975e83c53dde85ad2df84ff1a13
EX0PH1US <ex0ph1us2007@gmail.com>
2026-06-23 13:58:04 +0530

    added 404

--- api.js
+++ api.js
@@ -49,6 +49,10 @@ try {
     console.error(err)
 }
 
+app.use ((req, res, next) => {
+    res.status(404).json({ error: "Not Found", message: `Cannot ${req.method} ${req.originalUrl} on this server.` })
+})
+
 app.use(errorHandler)
 
 app.listen(3000, () => {
